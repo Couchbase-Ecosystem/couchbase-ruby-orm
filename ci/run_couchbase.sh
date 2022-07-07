2c52505b05fed4cd90432ba73882e0ae3e3fda16
Gauthier Monserand <gauthier@squadracer.com>
2022-07-07 14:46:02 +0200

    Add env vars in CI

--- ci/run_couchbase.sh
+++ ci/run_couchbase.sh
@@ -7,7 +7,7 @@ sudo service couchbase-server status
 /opt/couchbase/bin/couchbase-cli cluster-init -c 127.0.0.1:8091 --cluster-username=admin --cluster-password=password --cluster-ramsize=320 --cluster-index-ramsize=256 --cluster-fts-ramsize=256 --services=data,index,query,fts
 sleep 5
 /opt/couchbase/bin/couchbase-cli server-info   -c 127.0.0.1:8091 -u admin -p password
-/opt/couchbase/bin/couchbase-cli bucket-create -c 127.0.0.1:8091 -u admin -p password --bucket=${COUCHBASE_BUCKET} --bucket-type=couchbase --bucket-ramsize=160 --bucket-replica=0 --wait
+/opt/couchbase/bin/couchbase-cli bucket-create -c 127.0.0.1:8091 -u admin -p password --bucket=$COUCHBASE_BUCKET --bucket-type=couchbase --bucket-ramsize=160 --bucket-replica=0 --wait
 sleep 1
-/opt/couchbase/bin/couchbase-cli user-manage   -c 127.0.0.1:8091 -u admin -p password --set --rbac-username ${COUCHBASE_USER} --rbac-password ${COUCHBASE_PASSWORD} --rbac-name "Auto Tester" --roles admin --auth-domain local
-curl http://admin:password@localhost:8093/query/service -d "statement=CREATE INDEX `default_type` ON `${COUCHBASE_BUCKET}`(`type`)"
+/opt/couchbase/bin/couchbase-cli user-manage   -c 127.0.0.1:8091 -u admin -p password --set --rbac-username $COUCHBASE_USER --rbac-password ${COUCHBASE_PASSWORD} --rbac-name "Auto Tester" --roles admin --auth-domain local
+curl http://admin:password@localhost:8093/query/service -d "statement=CREATE INDEX `default_type` ON `$COUCHBASE_BUCKET`(`type`)"
