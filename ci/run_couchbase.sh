6f9c7670cf7617db90b6c39cd87df82ef4a585e1
Gauthier Monserand <gauthier@squadracer.com>
2022-07-07 15:11:13 +0200

    Add escaping

--- ci/run_couchbase.sh
+++ ci/run_couchbase.sh
@@ -17,4 +17,4 @@ sleep 5
 /opt/couchbase/bin/couchbase-cli bucket-create -c 127.0.0.1:8091 -u admin -p password --bucket=$BUCKET --bucket-type=couchbase --bucket-ramsize=160 --bucket-replica=0 --wait
 sleep 1
 /opt/couchbase/bin/couchbase-cli user-manage   -c 127.0.0.1:8091 -u admin -p password --set --rbac-username $USER --rbac-password $PASSWORD --rbac-name "Auto Tester" --roles admin --auth-domain local
-curl http://admin:password@localhost:8093/query/service -d "statement=CREATE INDEX `default_type` ON `$BUCKET`(`type`)"
+curl http://admin:password@localhost:8093/query/service -d "statement=CREATE INDEX \`default_type\` ON \`$BUCKET\`(\`type\`)"
