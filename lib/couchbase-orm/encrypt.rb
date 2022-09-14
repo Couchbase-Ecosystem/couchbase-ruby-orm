7595d2c70f20736a67333db0c48cd532e7dbf9d7
Damien Voreiter <damien.voreiter@doctolib.com>
2022-09-14 18:33:49 +0200

    try fix ci starts_with -> start_with

--- lib/couchbase-orm/encrypt.rb
+++ lib/couchbase-orm/encrypt.rb
@@ -6,7 +6,7 @@ module CouchbaseOrm
 
         def encode_encrypted_attributes(attributes)
             attributes.clone.each do |key, value|
-                if key.to_s.starts_with?(TANKER_ENCRYPTED_PREFIX)
+                if key.to_s.start_with?(TANKER_ENCRYPTED_PREFIX)
                     attributes["encrypted$#{key}"] = {
                         alg: 'CB_MOBILE_CUSTOM',
                         ciphertext: value
@@ -19,7 +19,7 @@ module CouchbaseOrm
         def decode_encrypted_attributes(attributes)
             attributes.clone.each do |key, value|
                 key = key.to_s
-                if key.starts_with?('encrypted$')
+                if key.start_with?('encrypted$')
                     attributes.delete(key)
                     attributes[key.gsub('encrypted$', '')] = value[:ciphertext]
                 end
