772e154f3728674e288f80c8f2f89430d43da8b8
CedricCouton <cedric.couton@doctolib.com>
2023-04-12 11:22:59 +0200

    test and solve cases of find with multiple ids

--- lib/couchbase-orm/base.rb
+++ lib/couchbase-orm/base.rb
@@ -254,7 +254,7 @@ module CouchbaseOrm
                 records = quiet ? collection.get_multi(ids, transcoder: transcoder) : collection.get_multi!(ids, transcoder: transcoder)
                 CouchbaseOrm.logger.debug { "Base.find found(#{records})" }
                 records = records.zip(ids).map { |record, id|
-                    self.new(record, id: id) if record
+                    self.new(record, id: id) if record && record.error.nil?
                 }
                 records.compact!
                 ids.length > 1 ? records : records[0]
