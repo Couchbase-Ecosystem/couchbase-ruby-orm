6641be244a7f358c944bbf0f9fcc5ac32d2238bd
Pierre Merlin <pierre.merlin@doctolib.com>
2023-10-09 17:36:55 +0200

    rm useless instance variables

--- lib/couchbase-orm/json_schema/json_validation_error.rb
+++ lib/couchbase-orm/json_schema/json_validation_error.rb
@@ -3,11 +3,8 @@
 module CouchbaseOrm
   module JsonSchema
     class JsonValidationError < StandardError
-      attr_reader :class_name, :errors
 
       def initialize(class_name, errors)
-        @class_name = class_name
-        @errors = errors
         super("[COUCHBASEORM]: Invalid document #{class_name} with errors : #{errors}")
       end
 
