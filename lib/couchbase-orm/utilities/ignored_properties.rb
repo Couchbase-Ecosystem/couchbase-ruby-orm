a13cd08db946c0c3b63cfcc1ad173f0c82053507
Pierre Merlin <pierre.merlin@doctolib.com>
2023-12-12 09:59:23 +0100

    format code

--- lib/couchbase-orm/utilities/ignored_properties.rb
+++ lib/couchbase-orm/utilities/ignored_properties.rb
@@ -1,11 +1,11 @@
 module CouchbaseOrm
-    module IgnoredProperties
-        def ignored_properties=(properties)
-            @ignored_properties = properties.map(&:to_s)
-        end
-
-        def ignored_properties(*args)
-            @ignored_properties ||= []
-        end
+  module IgnoredProperties
+    def ignored_properties=(properties)
+      @ignored_properties = properties.map(&:to_s)
     end
+
+    def ignored_properties(*_args)
+      @ignored_properties ||= []
+    end
+  end
 end
