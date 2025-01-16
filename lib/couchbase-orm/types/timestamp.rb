3d71024297df291f91624355691d2df3d8c9779b
Pierre Merlin <pierre.merlin@doctolib.com>
2025-01-16 16:37:05 +0100

    Apply precision on timestamp for any input type
    And keep unchanged the serialize_value needed for queries (ie find_by_some_time)

--- lib/couchbase-orm/types/timestamp.rb
+++ lib/couchbase-orm/types/timestamp.rb
@@ -2,13 +2,20 @@ module CouchbaseOrm
     module Types
         class Timestamp < ActiveModel::Type::DateTime
             def cast(value)
-              value = super(value)
-              return nil if value.nil?
-              return Time.at(value) if value.is_a?(Integer) || value.is_a?(Float)
-              return Time.at(value.to_i) if value.is_a?(String) && value =~ /^[0-9]+$/
-              return value.utc if value.is_a?(Time)
+                return nil if value.nil?
+
+                value = if value.is_a?(Integer) || value.is_a?(Float)
+                    Time.at(value)
+                elsif value.is_a?(String) && value =~ /^[0-9]+$/
+                    Time.at(value.to_i)
+                elsif value.is_a?(Time)
+                    value.utc
+                else
+                    value
+                end
+                super(value)
             end
-        
+
             def serialize(value)
                 value&.to_i
             end
--- lib/couchbase-orm/utilities/query_helper.rb
+++ lib/couchbase-orm/utilities/query_helper.rb
@@ -117,13 +117,13 @@ module CouchbaseOrm
             end
 
             def serialize_value(key, value_before_type_cast)
-                value = 
+                value =
                     if value_before_type_cast.is_a?(Array)
                         value_before_type_cast.map do |v|
-                            attribute_types[key.to_s].serialize(v)
+                            attribute_types[key.to_s].serialize(attribute_types[key.to_s].cast(v))
                         end
                     else
-                        attribute_types[key.to_s].serialize(value_before_type_cast)
+                        attribute_types[key.to_s].serialize(attribute_types[key.to_s].cast(value_before_type_cast))
                     end
                 CouchbaseOrm.logger.debug { "convert_values: #{key} => #{value_before_type_cast.inspect} => #{value.inspect} #{value.class} #{attribute_types[key.to_s]}" }
                 value
