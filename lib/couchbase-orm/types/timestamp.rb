5c56c7744b943b9e33cc0579ad935afb0895b8c0
Pierre Merlin <pierre.merlin@doctolib.com>
2025-06-24 17:34:32 +0200

    undo unwanted changes

--- lib/couchbase-orm/types/timestamp.rb
+++ lib/couchbase-orm/types/timestamp.rb
@@ -2,20 +2,13 @@ module CouchbaseOrm
     module Types
         class Timestamp < ActiveModel::Type::DateTime
             def cast(value)
-                return nil if value.nil?
-
-                value = if value.is_a?(Integer) || value.is_a?(Float)
-                    Time.at(value)
-                elsif value.is_a?(String) && value =~ /^[0-9]+$/
-                    Time.at(value.to_i)
-                elsif value.is_a?(Time)
-                    value.utc
-                else
-                    value
-                end
-                super(value)
+              return nil if value.nil?
+              return Time.at(value) if value.is_a?(Integer) || value.is_a?(Float)
+              return Time.at(value.to_i) if value.is_a?(String) && value =~ /^[0-9]+$/
+              return value.utc if value.is_a?(Time)
+              super(value).utc
             end
-
+        
             def serialize(value)
                 value&.to_i
             end
